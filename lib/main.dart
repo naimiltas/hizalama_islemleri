ee747db721569ae390272ed77854b5f46468354b
naimiltas <naimiltas@gmail.com>
2022-07-15 15:10:39 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        Container(
+            width: 100,
+            height: 100,
+            color: const Color.fromARGB(255, 47, 172, 194)),
+        Container(
+            width: 100,
+            height: 100,
+            color: const Color.fromARGB(255, 102, 23, 181)),
+        Container(
+            width: 100,
+            height: 100,
+            color: const Color.fromARGB(255, 15, 99, 224)),
+      ]),
+    );
+  }
+}
